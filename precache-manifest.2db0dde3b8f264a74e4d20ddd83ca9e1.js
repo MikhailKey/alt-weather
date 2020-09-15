d4529c85a072d5cc16c97842969e7699d0cacf3d
MikhailKey <48489639+MikhailKey@users.noreply.github.com>
2020-09-15 11:32:54 +0300

    Updates

---
+++ precache-manifest.2db0dde3b8f264a74e4d20ddd83ca9e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faf4ccfa670fbfbf0f1ce8e93c38b449",
+    "url": "/alt-weather/index.html"
+  },
+  {
+    "revision": "24a8320a85a2a49770c0",
+    "url": "/alt-weather/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "cad1718919017f5c8415",
+    "url": "/alt-weather/static/css/main.c2c835f9.chunk.css"
+  },
+  {
+    "revision": "24a8320a85a2a49770c0",
+    "url": "/alt-weather/static/js/2.a2f4257f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/alt-weather/static/js/2.a2f4257f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cad1718919017f5c8415",
+    "url": "/alt-weather/static/js/main.ebf21a09.chunk.js"
+  },
+  {
+    "revision": "d528932743d8c6412f82",
+    "url": "/alt-weather/static/js/runtime-main.a3a1e340.js"
+  }
+]);
